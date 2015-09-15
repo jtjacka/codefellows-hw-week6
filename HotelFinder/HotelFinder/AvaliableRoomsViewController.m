cfe0415abafa5f4a599ff171c5e17727511cce85
Jeffrey Jacka <jtjacka@gmail.com>
2015-09-14 22:13:31 -0700

    fetched results controller progress

--- HotelFinder/HotelFinder/AvaliableRoomsViewController.m
+++ HotelFinder/HotelFinder/AvaliableRoomsViewController.m
@@ -15,12 +15,11 @@
 #import "AppDelegate.h"
 #import "Hotel.h"
 
-@interface AvaliableRoomsViewController ()
+@interface AvaliableRoomsViewController () <NSFetchedResultsControllerDelegate>
 
 @property (strong, nonatomic) UITableView *tableView;
 @property (strong, nonatomic) NSArray *availableRooms;
-@property (strong, nonatomic) NSArray *hotels;
-@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (nonatomic, retain) NSFetchedResultsController *fetchedResultsController;
 
 @end
 
@@ -46,7 +45,6 @@
   [rootView addConstraints:tableViewHorizontalConstraints];
   
   self.availableRooms = [ReservationService avaliableRoomsForStartDate:self.startDate endDate:self.endDate];
-  [self retrieveHotelsFromReservation];
   
   self.tableView.dataSource = self;
   self.tableView.delegate = self;
@@ -57,9 +55,54 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
   
+  NSError *error;
+  if (![[self fetchedResultsController] performFetch:&error]) {
+    // Update to handle the error appropriately.
+    NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+    exit(-1);  // Fail
+  }
+  
+  self.title = @"Available Rooms";
 
   [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"Cell"];
     // Do any additional setup after loading the view.
+}
+
+//Ray Wenderlich Tutorial: NSFetchedResultsController
+//http://www.raywenderlich.com/999/core-data-tutorial-for-ios-how-to-use-nsfetchedresultscontroller
+- (NSFetchedResultsController *)fetchedResultsController {
+  
+  AppDelegate *appDelegate = [UIApplication sharedApplication].delegate;
+  
+  if (_fetchedResultsController != nil) {
+    return _fetchedResultsController;
+  }
+  
+  //Logic copied from AvaliableRoomService
+  NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+  NSPredicate *predicate = [NSPredicate predicateWithFormat:@"startDate <= %@ AND endDate >= %@", self.startDate, self.endDate];
+  
+  fetchRequest.predicate = predicate;
+
+  NSEntityDescription *entity = [NSEntityDescription
+                                 entityForName:@"Reservation" inManagedObjectContext:appDelegate.coreDataStack.managedObjectContext];
+  
+  [fetchRequest setEntity:entity];
+  
+  NSSortDescriptor *sort = [[NSSortDescriptor alloc] initWithKey:@"room.hotel" ascending:YES];
+  [fetchRequest setSortDescriptors:[NSArray arrayWithObject:sort]];
+  
+  [fetchRequest setFetchBatchSize:100];
+  
+  NSFetchedResultsController *theFetchedResultsController =
+  [[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest
+                                      managedObjectContext:appDelegate.coreDataStack.managedObjectContext sectionNameKeyPath:nil
+                                                 cacheName:@"Root"];
+  self.fetchedResultsController = theFetchedResultsController;
+  _fetchedResultsController.delegate = self;
+  
+  return _fetchedResultsController;
+  
 }
 
 - (void)didReceiveMemoryWarning {
@@ -67,42 +110,27 @@
     // Dispose of any resources that can be recreated.
 }
 
--(void)retrieveHotelsFromReservation {
-  self.hotels = [self.availableRooms valueForKeyPath:@"@distinctUnionOfObjects.hotel"];
+- (NSInteger)tableView:(UITableView *)tableView
+ numberOfRowsInSection:(NSInteger)section {
+  id  sectionInfo =
+  [[_fetchedResultsController sections] objectAtIndex:section];
+  return [sectionInfo numberOfObjects];
 }
 
-
-
--(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+-(void)configureCell:(UITableViewCell *)cell atIndexPath:(NSIndexPath *)indexPath {
   
-  return self.hotels.count;
-}
-
--(NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
-  Hotel  *currentHotel = self.hotels[section];
+  Room *avaliableRoom = [_fetchedResultsController objectAtIndexPath:indexPath];
+  cell.textLabel.text = [avaliableRoom.number stringValue];
   
-  return currentHotel.name;
-}
-
--(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-  Hotel  *currentHotel = self.hotels[section];
-  NSMutableArray *roomsInSection = [[NSMutableArray alloc] init];
-  
-  for (Room *index in self.availableRooms) {
-    if([currentHotel isEqual:index.hotel]) {
-      [roomsInSection addObject:index];
-    }
-  }
-  
-  return roomsInSection.count;
 }
 
 -(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-  UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
   
-  Room *avaliableRoom = self.availableRooms[indexPath.row];
+  static NSString *CellIdentifier = @"Cell";
   
-  cell.textLabel.text = [avaliableRoom.number stringValue];
+  UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+  
+  [self configureCell:cell atIndexPath:indexPath];
   
   return cell;
 }
@@ -124,6 +152,61 @@
   
   [self.navigationController pushViewController:destinationVC animated:true];
   
+}
+
+#pragma mark - NSFetchedResultsControllerDelegate
+- (void)controllerWillChangeContent:(NSFetchedResultsController *)controller {
+  // The fetch controller is about to start sending change notifications, so prepare the table view for updates.
+  [self.tableView beginUpdates];
+}
+
+
+- (void)controller:(NSFetchedResultsController *)controller didChangeObject:(id)anObject atIndexPath:(NSIndexPath *)indexPath forChangeType:(NSFetchedResultsChangeType)type newIndexPath:(NSIndexPath *)newIndexPath {
+  
+  UITableView *tableView = self.tableView;
+  
+  switch(type) {
+      
+    case NSFetchedResultsChangeInsert:
+      [tableView insertRowsAtIndexPaths:[NSArray arrayWithObject:newIndexPath] withRowAnimation:UITableViewRowAnimationFade];
+      break;
+      
+    case NSFetchedResultsChangeDelete:
+      [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
+      break;
+      
+    case NSFetchedResultsChangeUpdate:
+      [self configureCell:[tableView cellForRowAtIndexPath:indexPath] atIndexPath:indexPath];
+      break;
+      
+    case NSFetchedResultsChangeMove:
+      [tableView deleteRowsAtIndexPaths:[NSArray
+                                         arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
+      [tableView insertRowsAtIndexPaths:[NSArray
+                                         arrayWithObject:newIndexPath] withRowAnimation:UITableViewRowAnimationFade];
+      break;
+  }
+}
+
+
+- (void)controller:(NSFetchedResultsController *)controller didChangeSection:(id )sectionInfo atIndex:(NSUInteger)sectionIndex forChangeType:(NSFetchedResultsChangeType)type {
+  
+  switch(type) {
+      
+    case NSFetchedResultsChangeInsert:
+      [self.tableView insertSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationFade];
+      break;
+      
+    case NSFetchedResultsChangeDelete:
+      [self.tableView deleteSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationFade];
+      break;
+  }
+}
+
+
+- (void)controllerDidChangeContent:(NSFetchedResultsController *)controller {
+  // The fetch controller has sent all current change notifications, so tell the table view to process all updates.
+  [self.tableView endUpdates];
 }
 
 @end
